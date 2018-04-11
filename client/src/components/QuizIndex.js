f78caaa6a715ac9e0120d30c3505a3219b984c54
Bronislava Bernatikova <bronislava.bernatikova@gmail.com>
2018-04-10 20:02:32 -0700

    Add semanticUI into QuizIndex and SetUpNewQuiz

--- client/src/components/QuizIndex.js
+++ client/src/components/QuizIndex.js
@@ -1,40 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Table } from 'semantic-ui-react';
 
 function QuizIndex (props) {
   const { quizes = [] } = props;
 
-     return(
-      <div className="QuizIndex">
+  return(
+    <div className="QuizIndex"
+         style={{margin: '0 2rem'}}
+      >
+      <Table color="olive" key="olive" >
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell></Table.HeaderCell>
+            <Table.HeaderCell>Date</Table.HeaderCell>
+            <Table.HeaderCell>Expressions</Table.HeaderCell>
+            <Table.HeaderCell>Right Answers</Table.HeaderCell>
+            <Table.HeaderCell>Duration</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-        <table id="quizes-table">
-          <tbody>
-            <tr>
-              <th></th>
-              <th>Date</th>
-              <th>No Of Expressions</th>
-              <th>Right Answers</th>
-              <th>Time</th>
-            </tr>
-            {
-              quizes.map((quiz,index) => {
-              return(
-                <tr key={index} id={`row${index}`}>
-                    <td>{index + 1}</td>
-                    <td>{(quiz.date).slice(0,10)}</td>
-                    <td>{quiz.expression_count}</td>
-                    <td>{quiz.right_answer_count}</td>
-                    <td>{quiz.time}</td>
-                    <td>
-                      <Link to={`/quizes/show/${quiz.id}`}>Repeat Quiz</Link>
-                    </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
-     )
+        <Table.Body>
+          {
+            quizes.map((quiz,index) => {
+            return(
+              <Table.Row key={index} id={`row${index}`}>
+                <Table.Cell>{index + 1}</Table.Cell>
+                <Table.Cell>{(quiz.date).slice(0,10)}</Table.Cell>
+                <Table.Cell>{quiz.expression_count}</Table.Cell>
+                <Table.Cell>{quiz.right_answer_count}</Table.Cell>
+                <Table.Cell>{quiz.time}</Table.Cell>
+                <Table.Cell>
+                  <Link to={`/quizes/show/${quiz.id}`}>Repeat Quiz</Link>
+                </Table.Cell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  )
 }
-
 export default QuizIndex;
+
+
+
+
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+//
+// function QuizIndex (props) {
+//   const { quizes = [] } = props;
+//
+//      return(
+//       <div className="QuizIndex">
+//
+//         <table id="quizes-table">
+//           <tbody>
+//             <tr>
+//               <th></th>
+//               <th>Date</th>
+//               <th>No Of Expressions</th>
+//               <th>Right Answers</th>
+//               <th>Time</th>
+//             </tr>
+//             {
+//               quizes.map((quiz,index) => {
+//               return(
+//                 <tr key={index} id={`row${index}`}>
+//                     <td>{index + 1}</td>
+//                     <td>{(quiz.date).slice(0,10)}</td>
+//                     <td>{quiz.expression_count}</td>
+//                     <td>{quiz.right_answer_count}</td>
+//                     <td>{quiz.time}</td>
+//                     <td>
+//                       <Link to={`/quizes/show/${quiz.id}`}>Repeat Quiz</Link>
+//                     </td>
+//                 </tr>
+//               )
+//             })}
+//           </tbody>
+//         </table>
+//       </div>
+//      )
+// }
+//
+// export default QuizIndex;
--- client/src/components/SetUpNewQuiz.js
+++ client/src/components/SetUpNewQuiz.js
@@ -1,5 +1,27 @@
-
 import React, {Component} from 'react';
+
+import { Form, Button, Select } from 'semantic-ui-react';
+
+const numberOfExpressions = [
+  {key: '10', text: '10', value: '10' },
+  {key: '20', text: '20', value: '20' },
+  {key: '30', text: '30', value: '30' },
+  {key: '40', text: '40', value: '40' },
+  {key: '50', text: '50', value: '50' }
+];
+const arithmeticOperator = [
+  {key: 'add', text: 'Adding', value: 'add' },
+  {key: 'subtract', text: 'Subtraction', value: 'subtract' },
+  {key: 'multiply', text: 'Multiplication', value: 'multiply' },
+  {key: 'divide', text: 'Division', value: 'divide' }
+];
+const difficulty = [
+  {key: '100', text: 'Numbers up to 100', value: '100' },
+  {key: '300', text: 'Numbers up to 300', value: '300' },
+  {key: '500', text: 'Numbers up to 500', value: '500' },
+  {key: '700', text: 'Numbers up to 700', value: '700' },
+  {key: '1000', text: 'Numbers up to 1000', value: '1000' }
+];
 
 class SetUpNewQuiz extends Component {
 
@@ -16,20 +38,16 @@ class SetUpNewQuiz extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChangeNumberOfExpressions(event) {
-      this.setState({
-        numberOfExpressions: event.target.value
-      })
-    }
-  handleChangeArithmeticOperator(event) {
-    this.setState({
-      arithmeticOperator: event.target.value
-    })
+  handleChangeNumberOfExpressions = (event: React.SyntheticEvent<HTMLDivElement>, data: any) => {
+    this.setState({ numberOfExpressions: data.value });
   }
-  handleChangeDifficulty(event) {
-    this.setState({
-      difficulty: event.target.value
-    })
+
+  handleChangeArithmeticOperator = (event: React.SyntheticEvent<HTMLDivElement>, data: any) => {
+    this.setState({ arithmeticOperator: data.value });
+  }
+
+  handleChangeDifficulty = (event: React.SyntheticEvent<HTMLDivElement>, data: any) => {
+    this.setState({ difficulty: data.value });
   }
 
   handleSubmit (event) {
@@ -40,59 +58,39 @@ class SetUpNewQuiz extends Component {
       arithmeticOperator: this.state.arithmeticOperator,
       difficulty: this.state.difficulty
     };
+    console.log('newQuiz:', newQuiz);
     this.props.onSubmit(newQuiz);
   }
 
   render(){
+
     return (
-      <form className="SetUpNewQuiz"
+      <Form className="SetUpNewQuiz"
             onSubmit={this.handleSubmit}
+            style={{margin: '0 2rem'}}
       >
-        <div className="field">
-          <label>Number of expressions</label>
-          <select name="numberOfExpressions"
-                  value={this.state.numberOfExpressions}
-                  onChange={this.handleChangeNumberOfExpressions}
-          >
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-            <option value="40">40</option>
-            <option value="50">50</option>
-          </select>
-        </div>
-
-        <div className="field">
-          <label>Operator</label>
-          <select name="arithmeticOperator"
-                  value={this.state.arithmeticOperator}
-                  onChange={this.handleChangeArithmeticOperator}
-          >
-            <option value="add">Adding</option>
-            <option value="subtract">Subtraction</option>
-            <option value="multiply">Multiplycation</option>
-            <option value="divide">Division</option>
-          </select>
-        </div>
-
-        <div className="field">
-          <label>Difficulty</label>
-          <select name="difficulty"
-                  value={this.state.difficulty}
-                  onChange={this.handleChangeDifficulty}
-            >
-            <option value="10">Numbers up to 10</option>
-            <option value="100">Numbers up to 100</option>
-            <option value="300">Numbers up to 300</option>
-            <option value="500">Numbers up to 500</option>
-            <option value="700">Numbers up to 700</option>
-            <option value="1000">Numbers up to 1000</option>
-          </select>
-        </div>
-        <div>
-          <input type="submit" value="Submit"/>
-        </div>
-      </form>
+        <Form.Group widths='equal'>
+          <Form.Field control={Select}
+                      label='Number of expressions'
+                       options={numberOfExpressions}
+                       placeholder='Number of expressions'
+                       onChange={this.handleChangeNumberOfExpressions}
+                      />
+          <Form.Field control={Select}
+                      label='Operator'
+                       options={arithmeticOperator}
+                       placeholder='Operator'
+                       onChange={this.handleChangeArithmeticOperator}
+                      />
+          <Form.Field control={Select}
+                      label='Difficulty'
+                       options={difficulty}
+                       placeholder='Difficulty'
+                       onChange={this.handleChangeDifficulty}
+                      />
+        </Form.Group>
+        <Form.Field control={Button}>Create Quiz</Form.Field>
+      </Form>
     )}
 }
 export default SetUpNewQuiz;
